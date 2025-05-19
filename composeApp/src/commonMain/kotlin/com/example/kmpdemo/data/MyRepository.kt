e83cac02c63a1bfc67de945aa7c98618ece1d3fc
flight-simulator-australia <hongwei.bai84@gmail.com>
2025-05-19 23:37:06 +1000

    Add database sqldelight sample table

--- composeApp/src/commonMain/kotlin/com/example/kmpdemo/data/MyRepository.kt
+++ composeApp/src/commonMain/kotlin/com/example/kmpdemo/data/MyRepository.kt
@@ -8,4 +8,16 @@ class MyRepository(
     fun test() {
         println("test run!")
     }
+
+    fun insertUser(name: String, age: Int) {
+        dbHelper.insert(1, name, age, "test student")
+    }
+
+    fun clearAllUsers() {
+        dbHelper.clearAll()
+    }
+
+    fun getUserName(): String? {
+        return dbHelper.getRecordById(1)?.name
+    }
 }
