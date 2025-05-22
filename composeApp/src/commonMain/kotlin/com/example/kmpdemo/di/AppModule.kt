4bb53d49e7250ea22dda088fe796c113dc3c56d1
flight-simulator-australia <hongwei.bai84@gmail.com>
2025-05-22 22:28:38 +1000

    Add ViewModel demo

--- composeApp/src/commonMain/kotlin/com/example/kmpdemo/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/com/example/kmpdemo/di/AppModule.kt
@@ -4,9 +4,11 @@ import com.example.kmpdemo.data.MyRepository
 import org.koin.dsl.module
 import com.example.kmpdemo.database.DatabaseHelper
 import com.example.kmpdemo.domain.SomeUseCase
+import com.example.kmpdemo.ui.CounterViewModel
 
 val commonModule = module {
     single { DatabaseHelper(get()) }
     single { MyRepository(get()) }
     single { SomeUseCase(get()) }
+    factory { CounterViewModel(get()) }
 }
